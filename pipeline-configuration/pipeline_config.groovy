328db2c7d13f007f71207d265e30b57a40e38dcd
wittrupandersenn-ELS <97948388+wittrupandersenn-ELS@users.noreply.github.com>
2022-11-29 15:58:44 +0100

    Update pipeline_config.groovy

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -5,4 +5,12 @@ libraries{
 keywords{
   develop = ~/^[Dd]ev(elop|elopment|eloper|)$/
   main = ~/^[Mm](ain|aster)$/
+    defaults {
+      maven {
+                /*
+                 * Name of the Maven global settings.xml configuration file ID in Jenkins
+                 */
+                settingId = 'maven-settings-institutional-integrations-platform'
+            }
+    }
 }
