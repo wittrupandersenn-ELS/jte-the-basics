d32e42c1ac6341da1add4a9145ec6011493af9e8
wittrupandersenn-ELS <97948388+wittrupandersenn-ELS@users.noreply.github.com>
2022-11-29 13:19:45 +0100

    Update pipeline_config.groovy

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -6,13 +6,7 @@ libraries{
 
 allow_scm_jenkinsfile = true 
 
-
-application_environments{
-    dev{
-        ip_addresses = [ "0.0.0.1", "0.0.0.2" ]
-    }
-    prod{
-        long_name = "Production"
-        ip_addresses = [ "0.0.1.1", "0.0.1.2", "0.0.1.3", "0.0.1.4" ]
-    }
+keywords{
+  develop = ~/^[Dd]ev(elop|elopment|eloper|)$/
+  main = ~/^[Mm](ain|aster)$/
 }
